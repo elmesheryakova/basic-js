ef4ef849a9682fc62df19a85249f76c7fd1b984b
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-04-30 15:22:56 +0300

    feat: mac-adress & get-domain & matrix-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let res = []
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1] === undefined || (matrix[i - 1][j] !== 0)) {
+        res.push(matrix[i][j])
+      }
+    }
+  }
+  return res.reduce((acc, item) => acc + item)
 }
 
 module.exports = {
