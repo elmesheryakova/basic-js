fe69ecedede692be940878e589f14fa150f29ae8
elmesheryakova <e.meshcheryakova@dairyculture.ru>
2022-05-02 11:21:26 +0300

    feat: mine

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let res = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let line = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      if (i === 0) {
+        if (matrix[i][j - 1]) {
+          count++
+        }
+        if (matrix[i][j + 1]) {
+          count++
+        }
+        if (matrix[i + 1][j]) {
+          count++
+        }
+        if (matrix[i + 1][j - 1]) {
+          count++
+        }
+        if (matrix[i + 1][j + 1]) {
+          count++
+        }
+        count
+      } else {
+        if (matrix[i][j - 1]) {
+          count++
+        }
+        if (matrix[i][j + 1]) {
+          count++
+        }
+        if (matrix[i - 1][j]) {
+          count++
+        }
+        if (matrix[i - 1][j - 1]) {
+          count++
+        } if (matrix[i - 1][j + 1]) {
+          count++
+        }
+        count
+      }
+      line.push(count);
+    }
+    res.push(line);
+  }
+  return res;
 }
 
 module.exports = {
